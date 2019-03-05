8a51906f096f266fb71e7e6e2f358905de7f9d2e
Tanyamol <47579101+Tanyamol@users.noreply.github.com>
2019-03-05 19:58:36 +0200

    Homework_4

---
+++ Homework_4/src/com/epam/Task2.java
@@ -0,0 +1,22 @@
+package com.epam;
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        System.out.println("Input string: ");
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(lineProcessing(str));
+    }
+    public static String lineProcessing(String str){
+        String strNew="";
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(strNew.contains(String.valueOf(ch))){
+                continue;
+            } else strNew+=ch;
+        }
+              return strNew;
+}
+}
