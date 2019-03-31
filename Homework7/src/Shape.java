bc889a1fc04022509cc3b5966b553a2f0d682385
Tanyamol <47579101+Tanyamol@users.noreply.github.com>
2019-03-31 23:31:48 +0300

    Homework_7

---
+++ Homework7/src/Shape.java
@@ -0,0 +1,12 @@
+public class Shape {
+    protected String color;
+    public Shape(String color) {
+        this.color = color;
+    }
+    public String toString(){
+        return "class = " + this.getClass() + ":" + " color = " + color;
+    }
+    public double calcArea(){
+        return 0.0;
+    }
+}
